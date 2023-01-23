490711be71d3b2d84f575f1c79f89fa03f80c6b3
WAYPIL <ohs3051009@gmail.com>
2023-01-23 20:38:49 +0900

    Static → Dynamic (Flask)

---
+++ app.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')  # 서버에 파일 요청, 서버에 있던 HTML 파일의 코드를 모두 실행하기
+
+"""
+@app.route('/mypage')
+def mypage():
+   return '내 페이지!'
+"""
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
